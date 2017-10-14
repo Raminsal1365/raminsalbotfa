c07fa4b6258cba00059cbc0f9b8a75255122d8f4
Raminsal1365 <32774746+Raminsal1365@users.noreply.github.com>
2017-10-14 15:49:36 +0330

    Create config.lua

---
+++ data/config.lua
@@ -0,0 +1,26 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "banhammer-fa",
+    "groupmanager",
+    "groupmanager-fa",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "expiretime",
+    "mute-time",
+    "del",
+    "lock-fosh"
+  },
+  info_text = "م€‹permag.ir v3\n\nًں”°Our channel :\nم€‹@permag_botsم€ٹ\n\nًں”°Our channel2 :\nم€‹@permag_irم€ٹ\n\nًں”°Our channel3 :\nم€‹@permagم€ٹ\n\nًں”°Our website :\nم€‹http://permag.ir\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    415032467
+  }
+}
+return _
+end
